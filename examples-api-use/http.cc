2827129951b0cd1db143cc22cbb432b2e8902b8e
root <root@DESKTOP-M7SVLBT>
2025-01-02 22:13:24 +0200

    Remove draw

--- examples-api-use/http.cc
+++ examples-api-use/http.cc
@@ -22,29 +22,6 @@ static void InterruptHandler(int signo)
   interrupt_received = true;
 }
 
-static void DrawOnCanvas(Canvas *canvas)
-{
-  /*
-   * Let's create a simple animation. We use the canvas to draw
-   * pixels. We wait between each step to have a slower animation.
-   */
-  canvas->Fill(0, 0, 255);
-
-  int center_x = canvas->width() / 2;
-  int center_y = canvas->height() / 2;
-  float radius_max = canvas->width() / 2;
-  float angle_step = 1.0 / 360;
-  for (float a = 0, r = 0; r < radius_max; a += angle_step, r += angle_step)
-  {
-    if (interrupt_received)
-      return;
-    float dot_x = cos(a * 2 * M_PI) * r;
-    float dot_y = sin(a * 2 * M_PI) * r;
-    canvas->SetPixel(center_x + dot_x, center_y + dot_y, 255, 0, 0);
-    usleep(1 * 1000); // wait a little to slow down things.
-  }
-}
-
 std::vector<std::string> split(const std::string &s, const std::string &delimiter)
 {
   size_t last = 0;
@@ -82,8 +59,6 @@ int main(int argc, char *argv[])
   // for that.
   signal(SIGTERM, InterruptHandler);
   signal(SIGINT, InterruptHandler);
-
-  DrawOnCanvas(canvas); // Using the canvas.
 
   using namespace httplib;
 
