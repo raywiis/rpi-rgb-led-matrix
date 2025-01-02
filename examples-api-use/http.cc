8d501624961af9a9aa4eec4a5bc99d79f6f2cefa
rejus <rejus.paukst@gmail.com>
2025-01-02 21:27:51 +0200

    initial http

--- examples-api-use/http.cc
+++ examples-api-use/http.cc
@@ -6,7 +6,7 @@
 // (but note, that the led-matrix library this depends on is GPL v2)
 
 #include "led-matrix.h"
-#include "../../Code/cpp-httplib/httplib.h"
+#include "httplib.h"
 
 #include <unistd.h>
 #include <math.h>
@@ -44,49 +44,14 @@ static void DrawOnCanvas(Canvas *canvas) {
 }
 
 int main(int argc, char *argv[]) {
-  using namespace httplib;
-
-  Server svr;
-
-  svr.Get("/hi", [](const Request& req, Response& res) {
-    res.set_content("Hello World!", "text/plain");
-  });
-
-  // Match the request path against a regular expression
-  // and extract its captures
-  svr.Get(R"(/numbers/(\d+))", [&](const Request& req, Response& res) {
-    auto numbers = req.matches[0];
-    res.set_content(numbers, "text/plain");
-  });
-
-  // Capture the second segment of the request path as "id" path param
-  svr.Get("/users/:id", [&](const Request& req, Response& res) {
-    auto user_id = req.path_params.at("id");
-    res.set_content(user_id, "text/plain");
-  });
-
-  // Extract values from HTTP headers and URL query params
-  svr.Get("/body-header-param", [](const Request& req, Response& res) {
-    if (req.has_header("Content-Length")) {
-      auto val = req.get_header_value("Content-Length");
-    }
-    if (req.has_param("key")) {
-      auto val = req.get_param_value("key");
-    }
-    res.set_content(req.body, "text/plain");
-  });
-
-  svr.Get("/stop", [&](const Request& req, Response& res) {
-    svr.stop();
-  });
-
-  svr.listen("localhost", 1233);
   RGBMatrix::Options defaults;
   defaults.hardware_mapping = "regular";  // or e.g. "adafruit-hat"
   defaults.rows = 32;
   defaults.chain_length = 1;
   defaults.parallel = 1;
   defaults.show_refresh_rate = true;
+  defaults.cols = 64;
+
   Canvas *canvas = RGBMatrix::CreateFromFlags(&argc, &argv, &defaults);
   if (canvas == NULL)
     return 1;
@@ -98,6 +63,29 @@ int main(int argc, char *argv[]) {
   signal(SIGINT, InterruptHandler);
 
   DrawOnCanvas(canvas);    // Using the canvas.
+
+  using namespace httplib;
+
+  Server svr;
+
+  svr.Get("/hi", [](const Request& req, Response& res) {
+    res.set_content("Hello World!", "text/plain");
+  });
+
+  svr.Post("/draw", [canvas](const Request& req, Response& res) {
+    canvas->SetPixel(0, 0, 255, 255, 255);
+    res.set_content("ok", "text/plain");
+  });
+
+  svr.Get("/stop", [&](const Request& req, Response& res) {
+    svr.stop();
+  });
+
+  int port = 1233;
+  svr.listen("localhost", port);
+
+  std::cout << "Listening on port: " << port << std::endl;
+
 
   // Animation finished. Shut down the RGB matrix.
   canvas->Clear();
