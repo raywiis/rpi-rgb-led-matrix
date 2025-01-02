1774227b24d1ab15b82f8c11262158d13976fa7b
root <root@DESKTOP-M7SVLBT>
2025-01-02 22:09:20 +0200

    wow

--- examples-api-use/http.cc
+++ examples-api-use/http.cc
@@ -13,16 +13,17 @@
 #include <stdio.h>
 #include <signal.h>
 
-using rgb_matrix::RGBMatrix;
 using rgb_matrix::Canvas;
+using rgb_matrix::RGBMatrix;
 
 volatile bool interrupt_received = false;
-static void InterruptHandler(int signo) {
+static void InterruptHandler(int signo)
+{
   interrupt_received = true;
 }
 
-
-static void DrawOnCanvas(Canvas *canvas) {
+static void DrawOnCanvas(Canvas *canvas)
+{
   /*
    * Let's create a simple animation. We use the canvas to draw
    * pixels. We wait between each step to have a slower animation.
@@ -33,7 +34,8 @@ static void DrawOnCanvas(Canvas *canvas) {
   int center_y = canvas->height() / 2;
   float radius_max = canvas->width() / 2;
   float angle_step = 1.0 / 360;
-  for (float a = 0, r = 0; r < radius_max; a += angle_step, r += angle_step) {
+  for (float a = 0, r = 0; r < radius_max; a += angle_step, r += angle_step)
+  {
     if (interrupt_received)
       return;
     float dot_x = cos(a * 2 * M_PI) * r;
@@ -43,9 +45,28 @@ static void DrawOnCanvas(Canvas *canvas) {
   }
 }
 
-int main(int argc, char *argv[]) {
+std::vector<std::string> split(const std::string &s, const std::string &delimiter)
+{
+  size_t last = 0;
+  size_t next = 0;
+  std::vector<std::string> tokens;
+  std::string token;
+
+  while ((next = s.find(delimiter, last)) != std::string::npos)
+  {
+    token = s.substr(last, next - last);
+    tokens.push_back(token);
+    last = next + 1;
+  }
+  tokens.push_back(s.substr(last));
+
+  return tokens;
+}
+
+int main(int argc, char *argv[])
+{
   RGBMatrix::Options defaults;
-  defaults.hardware_mapping = "regular";  // or e.g. "adafruit-hat"
+  defaults.hardware_mapping = "regular"; // or e.g. "adafruit-hat"
   defaults.rows = 32;
   defaults.chain_length = 1;
   defaults.parallel = 1;
@@ -62,47 +83,45 @@ int main(int argc, char *argv[]) {
   signal(SIGTERM, InterruptHandler);
   signal(SIGINT, InterruptHandler);
 
-  DrawOnCanvas(canvas);    // Using the canvas.
+  DrawOnCanvas(canvas); // Using the canvas.
 
   using namespace httplib;
 
   Server svr;
 
-  svr.Get("/hi", [](const Request& req, Response& res) {
-    res.set_content("Hello World!", "text/plain");
-  });
+  svr.Get("/hi", [](const Request &req, Response &res)
+          { res.set_content("Hello World!", "text/plain"); });
 
-  svr.Post("/draw", [canvas](const Request& req, Response& res) {
+  svr.Post("/draw", [canvas](const Request &req, Response &res)
+           {
     canvas->SetPixel(0, 0, 255, 255, 255);
     std::size_t bodySize = res.body.size();
+    auto values = split(res.body, ",");
     int canvasSize = canvas->height() * canvas->width();
-    if (bodySize != (canvasSize * 3)) {
+    if (values.size() != (canvasSize * 3)) {
       res.set_content("size not ok", "text/plain");
       return;
     }
     canvas->Clear();
     for (int i = 0; i < canvasSize; i++) {
       int pos = i * 3;
-      auto& r = res.body.at(pos);
-      auto& g = res.body.at(pos + 1);
-      auto& b = res.body.at(pos + 2);
+      int r = std::stoi(values.at(pos));
+      int g = std::stoi(values.at(pos + 1));
+      int b = std::stoi(values.at(pos + 2));
 
       int x = i / canvas->width();
       int y = i % canvas->width();
       canvas->SetPixel(x, y, r, g, b);
     }
-    res.set_content("ok", "text/plain");
-  });
+    res.set_content("ok", "text/plain"); });
 
-  svr.Get("/stop", [&](const Request& req, Response& res) {
-    svr.stop();
-  });
+  svr.Get("/stop", [&](const Request &req, Response &res)
+          { svr.stop(); });
 
   int port = 1233;
   svr.listen("localhost", port);
 
   std::cout << "Listening on port: " << port << std::endl;
-
 
   // Animation finished. Shut down the RGB matrix.
   canvas->Clear();
